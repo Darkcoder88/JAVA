887fd3345194f195005b5d830649b42f45e84a3a
sunny samson <119066944+Darkcoder88@users.noreply.github.com>
2025-11-13 21:19:26 +0530

    Add files via upload

---
+++ VARIABLE/plus.java
@@ -0,0 +1,10 @@
+public class plus{
+    public static void main(String[] args){
+        System.out.println("the sum of these numbers is : ");
+        int num1 = 9;
+        int num2 = 5;
+        int num3 = 4;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+    }
+}
