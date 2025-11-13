c13c6de2270a4183bf6aeaf49760902f3e63a6be
sunny samson <119066944+Darkcoder88@users.noreply.github.com>
2025-11-13 22:37:47 +0530

    Add files via upload

---
+++ LITERALS/lit.java
@@ -0,0 +1,17 @@
+public class lit{
+    public static void main(String[] args) {
+        byte age = 34;
+        int age2 = 56;
+        short age3 = 87;
+        long ageDino = 56666666666L;
+        char ch = 'A';
+        float f1 = 5.56f;
+        double d1 = 4.66;
+        boolean a = true;
+        String detail = "These is code of literals made by me ";
+        System.out.println(age);
+        String str = "Sunny";
+        System.out.println(str);
+        System.out.println(detail);
+    }
+}
