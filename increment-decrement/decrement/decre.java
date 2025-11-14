e3ced38a11bfb6193d1c63782dc0f0f4a8169e8a
sunny samson <119066944+Darkcoder88@users.noreply.github.com>
2025-11-14 20:04:22 +0530

    Add files via upload

---
+++ increment-decrement/decrement/decre.java
@@ -0,0 +1,9 @@
+ public class decre{
+    public static void main(String[] args) {
+       int j = 56;
+        System.out.println(--j);
+        System.out.println(j);
+        System.out.println(j--);
+        System.out.println(j);
+    }
+}
---
+++ increment-decrement/increment/incr.java
@@ -0,0 +1,11 @@
+public class incr{
+    public static void main(String[] args) {
+        int i = 64;
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(i);
+        System.out.println(i++);
+        System.out.println(i);
+    }
+}
