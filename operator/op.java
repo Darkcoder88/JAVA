19dd0f95ba69d8ebf6eea882ec9e948d1f897ac7
sunny samson <119066944+Darkcoder88@users.noreply.github.com>
2025-11-14 14:13:22 +0530

    Add files via upload

---
+++ operator/op.java
@@ -0,0 +1,17 @@
+public class op{
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 9 + a;
+        int c = 18 - a;
+        int d = 50 * a;
+        int e = 12 / a;
+        int f = 60 % a;
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(64<9);
+        System.out.println(10<88);
+    }
+}
