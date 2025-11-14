31bc8c5ae1d6f49209a17c6c1d12aae502880fac
sunny samson <119066944+Darkcoder88@users.noreply.github.com>
2025-11-14 21:39:32 +0530

    Add files via upload

---
+++ ifelse/ithereorthese.java
@@ -0,0 +1,13 @@
+public class ithereorthese{
+    public static void main(String[] args) {
+        int age = 30;
+        //(age!=)
+        //(age ==)
+        if(age>18){
+            System.out.println("yes you can vote!");
+        }
+        else{
+            System.out.println("no you can't vote!");
+        }
+    }
+}
