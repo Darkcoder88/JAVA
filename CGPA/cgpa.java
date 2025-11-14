d858f5c750e78fb4dda4f045b90167b9e3ac4168
sunny samson <119066944+Darkcoder88@users.noreply.github.com>
2025-11-14 13:29:21 +0530

    Add files via upload

---
+++ CGPA/cgpa.java
@@ -0,0 +1,9 @@
+public class cgpa{
+    public static void main(String[] args) {
+        float sub1 = 45;
+        float sub2 = 89;
+        float sub3 = 75;
+        float cgpa = (sub1 + sub2 + sub3)/3;
+        System.out.println(cgpa);
+    }
+}
