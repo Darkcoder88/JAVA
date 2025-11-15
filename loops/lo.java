b8cff080afedcf8c17573e9a840bb0d62b91c263
sunny samson <119066944+Darkcoder88@users.noreply.github.com>
2025-11-15 20:57:53 +0530

    Add files via upload

---
+++ loops/lo.java
@@ -0,0 +1,15 @@
+public class lo{
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+
+        System.out.println("Using loops");
+        int i = 1;
+        while(i<=3000){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("END");
+    }
+}
